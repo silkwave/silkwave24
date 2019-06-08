ef357f92707839a8d73523c67111c100c61faa6e
silkwave <silkwave24@gmail.com>
2019-06-08 19:33:50 +0900

    일자유틸

---
+++ src/test/testMain.java
@@ -0,0 +1,12 @@
+package test;
+
+import util.Util;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		System.out.println("getDay() " + Util.getDay());
+		System.out.println("addDay() " + Util.addDay("20190606", 1));
+	}
+
+}
